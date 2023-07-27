f84776774087749bcead696041247923d2728506
Gamemod13 <igorfetov1997@gmail.com>
2023-07-27 23:01:49 +0300

    <create implementation of cinema-app>

---
+++ src/main/java/cinema/app/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,36 @@
+package cinema.app.service.impl;
+
+import cinema.app.model.Role;
+import cinema.app.model.User;
+import cinema.app.service.AuthenticationService;
+import cinema.app.service.RoleService;
+import cinema.app.service.ShoppingCartService;
+import cinema.app.service.UserService;
+import java.util.Collections;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    private final UserService userService;
+    private final RoleService roleService;
+    private final ShoppingCartService shoppingCartService;
+
+    public AuthenticationServiceImpl(UserService userService,
+                                     RoleService roleService,
+                                     ShoppingCartService shoppingCartService) {
+        this.userService = userService;
+        this.roleService = roleService;
+        this.shoppingCartService = shoppingCartService;
+    }
+
+    @Override
+    public User register(String email, String password) {
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(password);
+        user.setRoles(Collections.singleton(roleService.getByName(Role.RoleName.USER.name())));
+        userService.add(user);
+        shoppingCartService.registerNewShoppingCart(user);
+        return user;
+    }
+}
